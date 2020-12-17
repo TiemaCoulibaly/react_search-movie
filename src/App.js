4bb12539cc9c41e929e233fa5bca0a5836c45a83
TiemaCoulibaly <47168830+TiemaCoulibaly@users.noreply.github.com>
2020-12-17 16:01:14 +0100

    setting up the components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';	
+import Header from './components/Header';
+import SearchMovie from './components/SearchMovie';
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <SearchMovie/>
+    </>
+  )
+}
+export default App;
---
+++ src/components/SearchMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchMovie = () => {
+  return(
+    <>
+      <h1>Search Movie</h1>
+    </>
+  )
+}
+
+export default SearchMovie;
