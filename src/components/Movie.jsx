3ca5df758fa4be1b77d845c416ad1b72d51ad1fa
Tiema Coulibaly <tiemacouly@outlook.com>
2022-01-04 15:41:49 +0100

    adding icons into movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,7 +6,7 @@ const Movie = ({ id, title, poster_path, vote_average, release_date }) => {
 	return (
 		<>
 			<Link to={`/movie/${id}`}>
-				<div className="w-52 mx-4 rounded overflow-hidden shadow-md my-4 hover:shadow-2xl">
+				<div className="w-56 mx-4 rounded overflow-hidden shadow-md my-4 border hover:border-moon hover:shadow-2xl">
 					<img
 						className="w-full object-fill hover:scale-75"
 						src={
@@ -20,18 +20,33 @@ const Movie = ({ id, title, poster_path, vote_average, release_date }) => {
 						<h3 className="font-bold text-xl mb-2 text-white">
 							{title}
 						</h3>
-						<div className="flex flex-row">
-							<p className="font-bold text-xl mb-2 text-white">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-5 w-5 text-moon"
-									viewBox="0 0 20 20"
-									fill="currentColor">
-									<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-								</svg>
+						<div className="flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-7 w-7 text-moon"
+								viewBox="0 0 20 20"
+								fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<p className="font-bold text-xl mb-2 text-orange">
 								{vote_average}
 							</p>
-							<p className="font-bold text-xl mb-2 text-white">
+						</div>
+						<div className="flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-7 w-7 text-moon mr-2"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+								/>
+							</svg>
+							<p className="text-xl mb-2 text-white">
 								{release_date}
 							</p>
 						</div>
