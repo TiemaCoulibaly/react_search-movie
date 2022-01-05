c5fa823bb4fc430d4fc6c6f6676eb8a72388466c
Tiema Coulibaly <tiemacouly@outlook.com>
2022-01-05 09:40:53 +0100

    update env

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,20 @@ const Navbar = (props) => {
 		<>
 			<nav className="w-full flex flex-row items-center p-3 justify-around bg-night shadow-xs border-b-2 border-moon ">
 				<div className="ml-8 text-lg text-white hidden md:flex">
-					<Link to="/"> React Search Movie</Link>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6 mr-2"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
+						/>
+					</svg>{" "}
+					<Link to="/">React Search Movie</Link>
 				</div>
 				<span className="w-full md:w-3/12 h-10 bg-gray-200 cursor-pointer text-sm rounded-full flex">
 					<input
