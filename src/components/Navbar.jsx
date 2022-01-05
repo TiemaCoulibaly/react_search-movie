0b0c0ac637b7da6e89bae8cfbdd1710d4fa65dd8
Tiema Coulibaly <tiemacouly@outlook.com>
2022-01-05 10:08:50 +0100

    adding icon into navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,23 +5,25 @@ const Navbar = (props) => {
 	return (
 		<>
 			<nav className="w-full flex flex-row items-center p-3 justify-around bg-night shadow-xs border-b-2 border-moon ">
-				<div className="ml-8 text-lg text-white hidden md:flex">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6 mr-2"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor">
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth={2}
-							d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
-						/>
-					</svg>{" "}
-					<Link to="/">React Search Movie</Link>
+				<div className="ml-8 text-lg text-white flex flex-row lg:flex">
+					<Link to="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6 mr-2"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
+							/>
+						</svg>{" "}
+						<span>React Movie</span>
+					</Link>
 				</div>
-				<span className="w-full md:w-3/12 h-10 bg-gray-200 cursor-pointer text-sm rounded-full flex">
+				<span className="w-2/3 lg:w-3/12 md:w-3/12 h-10 bg-gray-200 cursor-pointer text-sm rounded-full flex">
 					<input
 						type="search"
 						name="search"
